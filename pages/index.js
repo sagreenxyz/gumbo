9e3a31330c72677b446c153776d29cbdb59ae783
SAGreenXYZ <sagreenxyz@gmail.com>
2022-07-26 16:26:09 -0500

    simple button

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
-import Link from 'next/link'
+import { Button } from 'semantic-ui-react'
+
+const ButtonExampleButton = () => <Button>Click Here</Button>
 
 export default function Home() {
     return (
-        <h1>Home</h1>
+        <>
+            <h1>Home</h1>
+            <ButtonExampleButton />
+        </>
     )
 }
