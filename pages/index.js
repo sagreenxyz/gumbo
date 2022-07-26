a09fe0f3f9a96d25f003fa6d98b0f00795132923
SAGreenXYZ <sagreenxyz@gmail.com>
2022-07-26 17:26:40 -0500

    example of login and logout in top menu with toggle; no underlying auth at the moment

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,17 @@
+import 'semantic-ui-css/semantic.min.css'
 import { Button } from 'semantic-ui-react'
+import { useState } from 'react'
+import Logins from '../components/Logins'
 
-const ButtonExampleButton = () => <Button>Click Here</Button>
-
-export default function Home() {
+export default function Home({ loggedIn = false }) {
+    let [visibility, setVisibility] = useState(false)
+    const handleToggle = () => {
+        setVisibility(!visibility)
+    }
     return (
         <>
-            <h1>Home</h1>
-            <ButtonExampleButton />
+            <Logins loggedIn={visibility} />
+            <Button onClick={handleToggle}>Toggle Logged In/Out</Button>
         </>
     )
 }
