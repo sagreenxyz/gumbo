7f15cf580d6904bbe21ae4abb24ba0dc0439bab3
SAGreenXYZ <sagreenxyz@gmail.com>
2022-07-26 16:20:52 -0500

    reset

--- pages/posts/[id]/[comment].js
+++
@@ -1,9 +0,0 @@
-import { useRouter } from 'next/router'
-
-const Comment = () => {
-    const router = useRouter()
-    const { comment } = router.query
-    return <p>Comment: {comment}</p>
-}
-
-export default Comment
