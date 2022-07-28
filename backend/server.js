0c5daf4b3b44f7f08e030256e27ceb52699fa32c
SAGreenXYZ <sagreenxyz@gmail.com>
2022-07-27 21:13:58 -0500

    stub for express server

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,8 @@
-const apikey = require('./utils/apikey')
+// const apikey = require('./utils/apikey')
 
-console.log(apikey.getAPIKey())
+// console.log(apikey.getAPIKey())
+
+const express = require('express')
+const app = express()
+
+app.listen(3000, () => {console.log(`server running at http://localhost:3000`)})
