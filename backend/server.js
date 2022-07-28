ca33c566734d2993edd3497329f40599d739d02e
SAGreenXYZ <sagreenxyz@gmail.com>
2022-07-27 21:17:53 -0500

    introducing root get handler

--- backend/server.js
+++ backend/server.js
@@ -7,4 +7,10 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello, World!'
+    })
+})
+
 app.listen(process.env.PORT, () => {console.log(`server running at http://localhost:${process.env.PORT}`)})
